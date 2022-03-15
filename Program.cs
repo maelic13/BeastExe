943eb00c04b9250a20640f1d58d65f9c544ae409
Miloslav Macůrek <macurek.m@gmail.com>
2022-03-15 13:46:29 +0100

    Compile BeastExe

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace BeastExe
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            var process = new Process {
+                StartInfo = new ProcessStartInfo {
+                    FileName = "./venv/scripts/python.exe",
+                    Arguments = "./src/main.py",
+                    UseShellExecute = false
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
